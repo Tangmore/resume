d898bad6e0b3f56c63f303ceb1d43b156c834dd5
tangmore <tangmore@163.com>
2018-12-04 00:52:42 +0800

    skills

--- js/index.js
+++ js/index.js
@@ -19,24 +19,23 @@ $(function(){
     // }
 
     tabToggle();
+
 })
 
 function tabToggle() {
-    var oTab = document.getElementsByClassName('categories')[0];
-    var aLi = oTab.getElementsByTagName('span');
-    var aBox = document.querySelectorAll('contentBox>div');
+    var aLi = document.querySelectorAll('.categories>span');
+    var aBox = document.querySelectorAll('.contentBox>div');
     for (var i = 0; i < aLi.length; i++) {
         aLi[i].index = i;
         aLi[i].onclick = function () {    
             for (var j = 0; j < aLi.length; j++) {
                 aLi[j].className=aLi[j].className.replace('selected','');//去掉所有li的selected样式
-                aBox[j].className = 'hide';//隐藏所有的tabDiv
+                aBox[j].className = ' hide';//隐藏所有的tabDiv
             }
             aLi[this.index].className+=' selected';
             aBox[this.index].className = '';
         }
     }
 }
-
 
 
